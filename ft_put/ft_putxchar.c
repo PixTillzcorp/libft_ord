9282974d0f76b661ed114ce6b9a1f0473468fc4a
Hippolyte Einfalt <hippolyteeinfalt@Hippolytes-MacBook-Pro.local>
2017-06-28 16:54:05 +0200

    sdqwdqw

--- ft_put/ft_putxchar.c
+++ ft_put/ft_putxchar.c
@@ -1,13 +1,18 @@
 #include "../libft.h"
 
-void	ft_putxchar(char c, int x)
+int		ft_putxchar(char c, int x)
 {
+	int ret;
+
 	if (x > 0)
 	{
+		ret = x;
 		while (x != 0)
 		{
 			write(1, &c, 1);
 			x--;
 		}
+		return (ret);
 	}
+	return (-1);
 }
